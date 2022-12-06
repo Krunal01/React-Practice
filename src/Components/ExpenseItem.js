6dc7c64bd8f0f319ce0a701f3ae7306177e30171
Krunal01 <parmarkrunal09101@gmail.com>
2022-12-06 17:49:49 +0530

    expenseitem css almost done

---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,19 @@
+import "./ExpenseItem.css";
+
+function ExpenseItem() {
+  return (
+    <div className="expenseitem">
+      <div className="expenseitem-date">6th December 2022</div>
+      <div className="expenseitem-name">Car Maintenance</div>
+      <div className="expenseitem-cost">$200</div>
+    </div>
+  );
+}
+
+export default ExpenseItem;
+{
+  /* <div className="expenseitem-desc"> */
+}
+{
+  /* </div> */
+}
